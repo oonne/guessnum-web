71ccca71246033da9c9efd9917135c452b703c62
JAY <JAY@oonne.com>
2025-05-20 15:20:24 +0800

    feat: 标题

--- src/components/guess-number/guess-number.tsx
+++ src/components/guess-number/guess-number.tsx
@@ -7,7 +7,12 @@ const GuessNumber = async ({ max }: { max: number }) => {
   await connection();
   const number = randomWithin(max) + 1;
 
-  return <div>{number}</div>;
+  return (
+    <div>
+      <h1>Guess a number between 1 and {max}</h1>
+      <div>{number}</div>
+    </div>
+  );
 };
 
 export default GuessNumber;
